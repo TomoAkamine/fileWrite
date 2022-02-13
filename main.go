480ae60daa9dcbefef7721725e7b12dc03c4e529
Tomo Akamine <e205737@ie.u-ryukyu.ac.jp>
2022-02-14 02:07:46 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/TomoAkamine/fileWrite/fileWrite"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(fileWrite.Hello("TomoAkamine"))
+}
